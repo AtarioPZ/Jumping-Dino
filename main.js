0609aa61931241bcf37484709e19a3aad45e7cb4
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2022-02-18 12:27:41 +0530

    player jump and collision box

--- main.js
+++ main.js
@@ -2,5 +2,21 @@ var character = document.getElementById("player");
 var block = document.getElementById("enemy");
 
 function jump() {
-    character.classList.add("animate");
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setTimeout(function () {
+        character.classList.remove("animate");
+    }, 500);
 }
+
+var collision = setInterval(function () {
+    var playerTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft < 20 && blockLeft > 0 && playerTop > 130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("OVER");
+        window.location.reload();
+    }
+}, 10);
